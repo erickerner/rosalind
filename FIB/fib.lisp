0b836d5a313f8646590996da8b709af25baa83e3
Eric Kerner <erickerner@gmail.com>
2013-12-19 19:05:36 -0600

    Common Lisp solution to FIB

---
+++ FIB/fib.lisp
@@ -0,0 +1,25 @@
+; [http://rosalind.info/problems/fib/]
+
+(defvar *terms*
+	'(
+		(1 . 1)
+		(2 . 1)
+	)
+)
+
+(defun fib (n k)
+	(let ((term (cdr (assoc n *terms*))))
+		(when (null term)
+			(setf term (+ (fib (- n 1) k) (* k (fib (- n 2) k))))
+			(push (cons n term) *terms*))
+		term
+	)
+)
+
+(defun main ()
+	(princ
+		(fib (parse-integer (nth 0 *args*)) (parse-integer (nth 1 *args*)))
+	)
+)
+
+(main)
